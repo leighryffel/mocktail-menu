1b63e52c8b667f4c1b2a6097cc122b176f831090
Leigh Ryffel <leightryffel@gmail.com>
2022-05-11 09:39:09 -0400

    added renderDrink function for top menu

--- src/index.js
+++ src/index.js
@@ -1,56 +1,13 @@
-//attempt 1 to append to div at top of page
-
-fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic')
-    .then(res => res.json())
-    .then(data => renderDrinks(data))
-
-
-
-    function renderDrinks (data) {
-        for(const element in data) {
-            console.log(data.key['strDrinkThumb'])
-
-
-        let drinkMenu = document.getElementById('drink-menu')
-       let cakeImages = data.strDrinkThumb
-        // drinkMenu.append()
-        }
-    
-    }
-
-
-
-
-
-
-
-
-
-
-
 // Pseudocode Outline:
 
-
 // define variables
+const drinkImageMenu = document.querySelector('#drink-menu')
+
 // write fetch request to collect data for all non-alcoholic drinks from cocktail API
-// fetch('www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic')
-//     .then(res => res.json())
-//     .then(data => getDrinkId(data))
+fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic')
+    .then(res => res.json())
+    .then(data => renderDrink(data))
 
-
-
-//      function getDrinkId(data) {
-//          let innerDrinks = Object.values(data)
-//          for(const e in (innerDrinks)) {
-//              console.log(e)
-//          }
-//         }
-     
-    
-    
-    
-    
-    
 // write .forEach() function to iterate through the non-alcoholic drinks and find their IDs, store in new array
 
 // write fetch request to collect data for the non-alcoholic IDs from cocktail API
@@ -59,8 +16,13 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic')
 
 // create function to put information from the fetched mocktails in the browser
 
-// Idea from the database creator:
-
-// Use the filters for non aloholic and create category buttons in your 
-// app. Then list the cocktails to browse, then do another lookup on the
-// ID once a user clicks on the cocktail for the details.
+function renderDrink(data) {
+    let drinksObj = Object.values(data);
+    let nestedDrinks = drinksObj[0];
+    for (let i = 0; i < nestedDrinks.length; i++) { 
+        const newImg = document.createElement('img');
+        newImg.className = "menu-image";
+        newImg.src = nestedDrinks[i].strDrinkThumb;
+        drinkImageMenu.appendChild(newImg);
+    };
+};
